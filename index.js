82347f99815760fd4f41586a0c3168f587fdb94e
unknown <ajayibcd@gmail.com>
2022-09-19 07:13:18 -0700

    added cors as middleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+
+
 const express = require('express');
+var cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes/api');
 require('dotenv').config();
 
 const app = express();
+app.use(cors())
 
 const port = process.env.PORT || 5000;
 
